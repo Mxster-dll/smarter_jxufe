39b1dfa97502345a06828dcf6a5589fc2d203eb5
Mxster-dll <Mxster.dll@outlook.com>
2026-04-19 15:05:01 +0800

    2.0.0.9 调整 `CollegeLocalDataSource` 和 `MajorLocalDataSource` 直接使用 `uuid` 作为 `key`

--- lib/features/college/data/college_local_datasource.dart
+++ lib/features/college/data/college_local_datasource.dart
@@ -7,25 +7,18 @@ class CollegeLocalDataSource {
 
   CollegeLocalDataSource(this._box);
 
-  String _keyString(String uuid) => 'College_$uuid';
-  String key(College college) => _keyString(college.uuid);
-
-  Future<void> saveCollege(College college) => _box.put(key(college), college);
+  Future<void> saveCollege(College college) => _box.put(college.uuid, college);
   Future<void> saveCollegeList(List<College> colleges) =>
-      Future.wait(colleges.map(saveCollege)); // TODO 看运行时是否报错
+      Future.wait(colleges.map(saveCollege));
 
-  Future<void> deleteCollege(String uuid) => _box.delete(_keyString(uuid));
+  bool contains(String uuid) => _box.containsKey(uuid);
+
+  College? getCollegeByUuid(String uuid) => _box.get(uuid);
+  List<College> getCollegeList() => _box.values.toList();
 
   List<College> findCollegeKnownAs(String name) =>
       _box.values.where((c) => c.isKnownAs(name)).toList();
 
-  // College? getCollegeByUuid(String uuid) => _box.get(key(college));
-
-  // List<T?> getCollegeListIn<T extends College>(FunctionType function) =>
-  //     _box.values.map((e) => e.modelIn<T>(function)).toList();
-  List<College> getCollegeList() => _box.values.toList();
-
-  bool contains(String uuid) => _box.containsKey(_keyString(uuid));
-
+  Future<void> deleteCollege(String uuid) => _box.delete(uuid);
   Future<int> clearAll() => _box.clear();
 }
--- lib/features/major/data/major_local_datasource.dart
+++ lib/features/major/data/major_local_datasource.dart
@@ -1,5 +1,4 @@
 import 'package:hive_flutter/hive_flutter.dart';
-
 import 'package:smarter_jxufe/features/major/domain/major.dart';
 
 class MajorLocalDataSource {
@@ -7,25 +6,18 @@ class MajorLocalDataSource {
 
   MajorLocalDataSource(this._box);
 
-  String _keyString(String uuid) => 'Major_$uuid';
-  String key(Major major) => _keyString(major.uuid);
-
-  Future<void> saveMajor(Major major) => _box.put(key(major), major);
+  Future<void> saveMajor(Major major) => _box.put(major.uuid, major);
   Future<void> saveMajorList(List<Major> majors) =>
-      Future.wait(majors.map(saveMajor)); // TODO 看运行时是否报错
+      Future.wait(majors.map(saveMajor));
 
-  Future<void> deleteMajor(String uuid) => _box.delete(_keyString(uuid));
+  bool contains(String uuid) => _box.containsKey(uuid);
+
+  Major? getMajorByUuid(String uuid) => _box.get(uuid);
+  List<Major> getMajorList() => _box.values.toList();
 
   List<Major> findMajorKnownAs(String name) =>
       _box.values.where((c) => c.isKnownAs(name)).toList();
 
-  // Major? getMajorByUuid(String uuid) => _box.get(key(major));
-
-  // List<T?> getMajorListIn<T extends Major>(FunctionType function) =>
-  //     _box.values.map((e) => e.modelIn<T>(function)).toList();
-  List<Major> getMajorList() => _box.values.toList();
-
-  bool contains(String uuid) => _box.containsKey(_keyString(uuid));
-
+  Future<void> deleteMajor(String uuid) => _box.delete(uuid);
   Future<int> clearAll() => _box.clear();
 }
