cb7be8625b06cbd5c3194e8d068ac21d65c6f14d
Mxster-dll <Mxster.dll@outlook.com>
2026-05-17 14:53:37 +0800

    2.2.0.0 课程表功能测试

---
+++ lib/features/ims/schedule/data/anti_corruption/schedule_html_parser.dart
@@ -0,0 +1,40 @@
+import 'package:html/dom.dart';
+import 'package:html/parser.dart' as parser;
+
+import 'package:smarter_jxufe/utils/Log.dart';
+
+class ScheduleHtmlParser {
+  List<List<String>> parse(String html) {
+    final document = parser.parse(html);
+    final tables = document.querySelectorAll('table');
+
+    if (tables.length != 2) {
+      logInfo(html);
+      throw Exception('期望有2个 table，但找到了${tables.length}个 table\n $tables');
+    }
+
+    final table = tables[1];
+
+    // 获取所有非表头行
+    final rows = table
+        .querySelectorAll('tr')
+        .where((tr) => !tr.classes.contains('H'))
+        .toList();
+    final List<List<String>> result = [];
+
+    for (var tr in rows) {
+      final tds = tr.children
+          .whereType<Element>()
+          .where((td) => !td.classes.contains('td1'))
+          .toList();
+
+      final rowCells = tds.map((td) => td.text.trim()).toList();
+
+      if (rowCells.isNotEmpty) {
+        result.add(rowCells);
+      }
+    }
+
+    return result;
+  }
+}
