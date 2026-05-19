3294f112fc131e20faae781d312c83665a489b10
Mxster-dll <Mxster.dll@outlook.com>
2026-05-19 21:22:29 +0800

    3.0.0.0 实现加载页，并实现初次登录后面的自动登录

--- lib/core/network/dio_providers.g.dart
+++ lib/core/network/dio_providers.g.dart
@@ -20,5 +20,19 @@ final imsDioProvider = AutoDisposeProvider<Dio>.internal(
 );
 
 typedef ImsDioRef = AutoDisposeProviderRef<Dio>;
+String _$loginDioHash() => r'61b9ed2106b15b17a98004da89935cc8a71a2ecc';
+
+/// See also [loginDio].
+@ProviderFor(loginDio)
+final loginDioProvider = AutoDisposeProvider<Dio>.internal(
+  loginDio,
+  name: r'loginDioProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$loginDioHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LoginDioRef = AutoDisposeProviderRef<Dio>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/features/auth/data/providers/auth_repository_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_repository_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authRepositoryHash() => r'3e014ee3f00539c6b3816d7e909a57f8c72d61bb';
+
+/// See also [authRepository].
+@ProviderFor(authRepository)
+final authRepositoryProvider = AutoDisposeProvider<AuthRepository>.internal(
+  authRepository,
+  name: r'authRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AuthRepositoryRef = AutoDisposeProviderRef<AuthRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
