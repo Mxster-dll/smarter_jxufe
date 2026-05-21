7f2384bfaf943f8b2ea9b2618fec271f6ff5152a
Mxster-dll <Mxster.dll@outlook.com>
2026-05-21 09:38:46 +0800

    3.1.0.6 将所有 `HiveBox` 的 `name` 统一管理

--- lib/features/ims/curriculum/data/providers/curriculum_box_provider.dart
+++ lib/features/ims/curriculum/data/providers/curriculum_box_provider.dart
@@ -1,10 +1,11 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import 'package:smarter_jxufe/core/constants/hive_box_names.dart';
 import 'package:smarter_jxufe/features/ims/curriculum/domain/curriculum.dart';
 
 part 'curriculum_box_provider.g.dart';
 
 @riverpod
 Future<Box<Curriculum>> curriculumBox(CurriculumBoxRef ref) =>
-    Hive.openBox<Curriculum>('curriculums');
+    Hive.openBox<Curriculum>(curriculumBoxName);
