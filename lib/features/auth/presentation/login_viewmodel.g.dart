9c773f5c76efcd971288e221819f09ffa869f39e
Mxster-dll <Mxster.dll@outlook.com>
2026-05-26 11:57:46 +0800

    3.3.1.0 将 `LoginViewModel` 的 `provider` 改为由代码生成实现

---
+++ lib/features/auth/presentation/login_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginViewModelHash() => r'd58581b2de5cdd2637c8c336ee740adc3ecebb15';
+
+/// See also [LoginViewModel].
+@ProviderFor(LoginViewModel)
+final loginViewModelProvider =
+    AutoDisposeNotifierProvider<LoginViewModel, LoginState>.internal(
+  LoginViewModel.new,
+  name: r'loginViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$loginViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LoginViewModel = AutoDisposeNotifier<LoginState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
